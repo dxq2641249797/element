d666b45efec65d6d9655f2c96cbd353364f17bda
duxiaoqi <duxiaoqi@hibaosheng.com>
2021-05-07 10:30:12 +0800

    new project

---
+++ src/views/home/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    HOME
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HOME',
+  components: {
+
+  },
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
